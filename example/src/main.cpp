052cf1b019cc76e325b509fe28242460c71b63b2
Tim Gfrerer <tim.gfrerer@oo43.com>
2012-10-08 16:22:08 +0100

    updated main.cpp
    glut string disabled by default
    
    Signed-off-by: Tim Gfrerer <tim.gfrerer@oo43.com>

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -6,7 +6,7 @@
 int main( ){
 
     ofAppGlutWindow window;
-	window.setGlutDisplayString("rgba double samples>=4 depth");
+	//	window.setGlutDisplayString("rgba double samples>=4 depth");
 
 	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
 
